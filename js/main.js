c8b574c495d93b9837dbbd38a30ca6e4761411c1
autumnly1007 <autumnly1007@gmail.com>
2022-11-06 21:33:11 +0900

    Add select, options

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
 const API_KEY = '7035c60c';
-let search = 'cat';
-let page = 1;
-let year = '';
+localStorage.setItem('likes', '[]');
 
 // 비동기
 (async () => {
-  const moreBtn = document.querySelector('.btn');
-  const searchBtn = document.querySelector('.search-btn');
-
-  // 영화 정보 가져오기
-  // const movies = await getMovies();
-
-  // 영화 정보 출력하기
-  // renderMovies(movies);
-
   // 영화 개봉연도 selectBox 생성
   createYearSelect();
 
   // 검색 버튼 클릭 이벤트
+  const searchBtn = document.querySelector('.search-btn');
   searchBtn.addEventListener('click', async () => {
-    const movies = await getMovies(page);
+    const movies = await getMovies();
     renderMovies(movies);
   });
 
@@ -31,67 +21,111 @@ let year = '';
       searchBtn.click();
     }
   });
-
-  // 더보기 버튼 클릭 이벤트
-  moreBtn.addEventListener('click', async () => {
-    const movies = await getMovies(page);
-    page += 1;
-    renderMovies(movies);
-  });
 })();
 
 // 영화 정보 가져오기
-async function getMovies(page = 1) {
+async function getMovies() {
   const search = document.querySelector('.search-input').value;
   if (!search) {
     alert('영화 제목을 입력해 주세요.');
     return;
   }
-  const year = document.querySelector('.year').value;
   const type = document.querySelector('.type').value;
-  document.querySelector('.movies').innerHTML = '';
-  const res = await fetch(`https://omdbapi.com/?apikey=${API_KEY}&s=${search}&type=${type}&y=${year}&page=${page}`);
-  const { Search: movies } = await res.json();
+  const year = document.querySelector('.year').value;
+  const pages = JSON.parse(document.querySelector('.page').value);
+  let movies = [];
+  for await (const page of pages) {
+    const res = await fetch(`https://omdbapi.com/?apikey=${API_KEY}&s=${search}&type=${type}&y=${year}&page=${page}`);
+    const { Search: moviesData } = await res.json();
+    if (moviesData) {
+      movies.push(...moviesData);
+    }
+  }
   return movies;
 }
 
 // 영화 정보 출력하기
 function renderMovies(movies) {
   const moviesEl = document.querySelector('.movies');
-  for (const movie of movies) {
-    const el = document.createElement('div');
-    el.classList.add('movie');
-
+  document.querySelector('.movies').innerHTML = '';
+  document.querySelector('.search-result').style.display = 'block';
+  if (movies.length === 0) {
     const h1El = document.createElement('h1');
-    h1El.textContent = movie.Title;
+    h1El.textContent = '검색 결과가 없습니다. 😢';
+    moviesEl.append(h1El);
+    return;
+  }
+  for (const movie of movies) {
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+    movieEl.id = movie.imdbID;
 
-    h1El.addEventListener('click', () => {
-      console.log(movie.Title);
+    movieEl.addEventListener('click', () => {
+      location.href = `detail.html/${movie.imdbID}`;
     });
 
-    const imgEl = document.createElement('img');
-    imgEl.src = movie.Poster;
+    const descEl = document.createElement('div');
+    descEl.className = 'desc';
+    const h1El = document.createElement('h1');
+    h1El.textContent = movie.Title;
+    const pEl = document.createElement('p');
+    pEl.textContent = movie.Year;
+    descEl.append(h1El, pEl);
 
-    el.append(imgEl, h1El);
-    moviesEl.append(el);
+    const imgEl = document.createElement('img');
+    imgEl.alt = movie.Title;
+    if (movie.Poster !== 'N/A') {
+      imgEl.src = movie.Poster;
+    } else {
+      imgEl.src = '/imgs/no-image.png';
+    }
+
+    const likeBtn = document.createElement('button');
+    likeBtn.classList.add('like-btn');
+
+    const spanEl = document.createElement('span');
+    spanEl.className = 'material-symbols-outlined';
+    spanEl.innerHTML = 'favorite';
+    const likes = JSON.parse(localStorage.getItem('likes'));
+    if (likes.includes(movie.imdbID)) {
+      spanEl.classList.add('like');
+    }
+
+    // 좋아요 버튼 클릭 이벤트
+    likeBtn.addEventListener('click', () => {
+      const likes = JSON.parse(localStorage.getItem('likes'));
+      if (!likes.includes(movie.imdbID)) {
+        likes.push(movie.imdbID);
+        localStorage.setItem('likes', JSON.stringify(likes));
+        spanEl.classList.add('like');
+      } else {
+        const idx = likes.indexOf(movie.imdbID);
+        likes.splice(idx, 1);
+        localStorage.setItem('likes', JSON.stringify(likes));
+        spanEl.classList.remove('like');
+      }
+    });
+    likeBtn.append(spanEl);
+
+    movieEl.append(imgEl, descEl, likeBtn);
+    moviesEl.append(movieEl);
   }
-  document.querySelector('.actions .btn').style.display = 'inline-block';
 }
 
 // 영화 개봉연도 selectBox 생성
 function createYearSelect() {
-  const select = document.createElement('select');
-  select.className = 'year';
-  const option = document.createElement('option');
-  option.innerHTML = 'All Years';
-  select.append(option);
+  const selectEl = document.createElement('select');
+  selectEl.className = 'year';
+  const optionEl = document.createElement('option');
+  optionEl.innerHTML = 'All Years';
+  selectEl.append(optionEl);
 
   const curYear = new Date().getFullYear();
   for (let i = curYear; i >= 1985; i--) {
-    const option = document.createElement('option');
-    option.innerHTML = i;
-    option.value = i;
-    select.append(option);
+    const optionEl = document.createElement('option');
+    optionEl.innerHTML = i;
+    optionEl.value = i;
+    selectEl.append(optionEl);
   }
-  document.querySelector('.search-options').prepend(select);
+  document.querySelector('.search-options').prepend(selectEl);
 }
